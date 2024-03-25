59788b4715a42f370faa30f77d2238d1b7bbf264
abysscat-yj <397967552@qq.com>
2024-03-26 00:39:59 +0800

    update pom

---
+++ catrpc-demo-consumer/src/test/java/com/abysscat/catrpc/demo/consumer/CatrpcDemoConsumerApplicationTest.java
@@ -0,0 +1,22 @@
+package com.abysscat.catrpc.demo.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Description
+ *
+ * @Author: abysscat-yj
+ * @Create: 2024/3/26 0:01
+ */
+@SpringBootTest
+class CatrpcDemoConsumerApplicationTest {
+
+	@Test
+	void contextLoad() {
+
+	}
+
+}
