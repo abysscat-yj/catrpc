87b70e7d2ed43c29b5e1d551372efa105221e10e
abysscat-yj <397967552@qq.com>
2024-04-25 03:17:24 +0800

    upgrade HttpInvoker

--- catrpc-core/src/main/java/com/abysscat/catrpc/core/consumer/HttpInvoker.java
+++ catrpc-core/src/main/java/com/abysscat/catrpc/core/consumer/HttpInvoker.java
@@ -2,6 +2,12 @@ package com.abysscat.catrpc.core.consumer;
 
 import com.abysscat.catrpc.core.api.RpcRequest;
 import com.abysscat.catrpc.core.api.RpcResponse;
+import com.abysscat.catrpc.core.consumer.http.OkHttpInvoker;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Description
@@ -11,6 +17,38 @@ import com.abysscat.catrpc.core.api.RpcResponse;
  */
 public interface HttpInvoker {
 
+	Logger log = LoggerFactory.getLogger(HttpInvoker.class);
+
+	HttpInvoker Default = new OkHttpInvoker(500);
+
 	RpcResponse<?> post(RpcRequest rpcRequest, String url);
+
+	String post(String requestString, String url);
+
+	String get(String url);
+
+	@SneakyThrows
+	static <T> T httpGet(String url, Class<T> clazz) {
+		log.debug(" =====>>>>>> httpGet: " + url);
+		String respJson = Default.get(url);
+		log.debug(" =====>>>>>> response: " + respJson);
+		return JSON.parseObject(respJson, clazz);
+	}
+
+	@SneakyThrows
+	static <T> T httpGet(String url, TypeReference<T> typeReference) {
+		log.debug(" =====>>>>>> httpGet: " + url);
+		String respJson = Default.get(url);
+		log.debug(" =====>>>>>> response: " + respJson);
+		return JSON.parseObject(respJson, typeReference);
+	}
+
+	@SneakyThrows
+	static <T> T httpPost(String requestString, String url, Class<T> clazz) {
+		log.debug(" =====>>>>>> httpGet: " + url);
+		String respJson = Default.post(requestString, url);
+		log.debug(" =====>>>>>> response: " + respJson);
+		return JSON.parseObject(respJson, clazz);
+	}
 
 }
--- catrpc-core/src/main/java/com/abysscat/catrpc/core/consumer/http/OkHttpInvoker.java
+++ catrpc-core/src/main/java/com/abysscat/catrpc/core/consumer/http/OkHttpInvoker.java
@@ -50,4 +50,36 @@ public class OkHttpInvoker implements HttpInvoker {
 			throw new RuntimeException(e);
 		}
 	}
+
+	@Override
+	public String post(String requestString, String url) {
+		log.debug(" ===> post  url = {}, requestString = {}", requestString, url);
+		Request request = new Request.Builder()
+				.url(url)
+				.post(RequestBody.create(requestString, JSON_TYPE))
+				.build();
+		try {
+			String respJson = client.newCall(request).execute().body().string();
+			log.debug(" ===> respJson = " + respJson);
+			return respJson;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public String get(String url) {
+		log.debug(" ===> get url = " + url);
+		Request request = new Request.Builder()
+				.url(url)
+				.get()
+				.build();
+		try {
+			String respJson = client.newCall(request).execute().body().string();
+			log.debug(" ===> respJson = " + respJson);
+			return respJson;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
